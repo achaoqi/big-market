f604d8b478018b5d50b06d29650afe5b06c689d3
smartchaoqi <qcq11111111@163.com>
2024-08-01 11:12:19 +0800

    feat:引入分库分表路由组件

---
+++ big-market-app/src/test/java/com/aqiu/test/domain/RaffleActivityDaoTest.java
@@ -0,0 +1,26 @@
+package com.aqiu.test.domain;
+
+import com.alibaba.fastjson.JSON;
+import com.aqiu.infrastructure.persistent.dao.IRaffleActivityDao;
+import com.aqiu.infrastructure.persistent.po.RaffleActivity;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class RaffleActivityDaoTest {
+    @Resource
+    private IRaffleActivityDao raffleActivityDao;
+
+    @Test
+    public void test_queryRaffleActivityByActivityId() {
+        RaffleActivity raffleActivity = raffleActivityDao.queryRaffleActivityByActivityId(100301L);
+        log.info("测试结果：{}", JSON.toJSONString(raffleActivity));
+    }
+}
---
+++ big-market-infrastructure/src/main/java/com/aqiu/infrastructure/persistent/dao/IRaffleActivityDao.java
@@ -0,0 +1,16 @@
+package com.aqiu.infrastructure.persistent.dao;
+
+import com.aqiu.infrastructure.persistent.po.RaffleActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Fuzhengwei bugstack.cn @小傅哥
+ * @description 抽奖活动表Dao
+ * @create 2024-03-09 10:04
+ */
+@Mapper
+public interface IRaffleActivityDao {
+
+    RaffleActivity queryRaffleActivityByActivityId(Long activityId);
+
+}
