d26cf5be7b194bb7b3f7e1ba3b0f8b530ada4d52
smartchaoqi <qcq11111111@163.com>
2024-08-04 09:34:24 +0800

    feat:引入MQ处理活动SKU库存一致性

---
+++ big-market-infrastructure/src/main/java/com/aqiu/infrastructure/persistent/dao/ITaskDao.java
@@ -0,0 +1,7 @@
+package com.aqiu.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ITaskDao {
+}
---
+++ big-market-infrastructure/src/main/java/com/aqiu/infrastructure/persistent/po/UserRaffleOrder.java
@@ -0,0 +1,29 @@
+package com.aqiu.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UserRaffleOrder {
+    /** 自增ID */
+    private String id;
+    /** 用户ID */
+    private String userId;
+    /** 活动ID */
+    private Long activityId;
+    /** 活动名称 */
+    private String activityName;
+    /** 抽奖策略ID */
+    private Long strategyId;
+    /** 订单ID */
+    private String orderId;
+    /** 下单时间 */
+    private Date orderTime;
+    /** 订单状态；create-创建、used-已使用、cancel-已作废 */
+    private String orderState;
+    /** 创建时间 */
+    private Date createTime;
+    /** 更新时间 */
+    private Date updateTime;
+}
